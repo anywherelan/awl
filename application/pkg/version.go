77431902020d5acb10c7be9f8b9c4ecd077c491d
Maxim Merzhanov <maksimmerzh@gmail.com>
2021-04-25 13:09:34 +0300

    bump version

--- application/pkg/version.go
+++ application/pkg/version.go
@@ -2,7 +2,7 @@ package pkg
 
 // TODO move to root package
 const (
-	Version         = "v0.3.2"
+	Version         = "v0.3.3"
 	UserAgent       = UserAgentPrefix + Version
 	UserAgentPrefix = "awl/"
 )
