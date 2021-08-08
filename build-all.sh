72a079be168c35c71b0717177530ec4914b67e3e
Maxim Merzhanov <maksimmerzh@gmail.com>
2021-08-08 23:14:41 +0300

    awl,awl-tray: embed wintun.dll in binary for windows

--- build-all.sh
+++ build-all.sh
@@ -10,23 +10,36 @@ rm -rf build/
 mkdir build
 rm -rf static/
 
+wintun_version="wintun-0.11"
+if [[ ! -a "/tmp/$wintun_version" ]]; then
+  wget "https://www.wintun.net/builds/$wintun_version.zip"
+  unzip "$wintun_version.zip" -d "/tmp/$wintun_version"
+  rm -f "$wintun_version.zip"
+fi
+
 gobuild() {
 	name="$1"
 	for arch in 386 amd64 arm arm64; do
 	  filename="$name-linux-$arch-$version"
-	  GOOS=linux GOARCH=$arch go build -o $filename
-	  mv $filename $builddir
+	  GOOS=linux GOARCH=$arch go build -o "$filename"
+	  mv "$filename" "$builddir"
   done
 
-	for arch in 386 amd64; do
-	  filename="$name-windows-$arch-$version.exe"
-	  GOOS=windows GOARCH=$arch go build -ldflags "-H windowsgui" -o $filename
-	  mv $filename $builddir
+  for tuple in "386 x86" "amd64 amd64"
+  do
+    goarch=$(echo "$tuple" | cut -f1 -d" ")
+    wintunarch=$(echo "$tuple" | cut -f2 -d" ")
+    cp "/tmp/$wintun_version/wintun/bin/$wintunarch/wintun.dll" wintun.dll
+
+	  filename="$name-windows-$goarch-$version.exe"
+	  GOOS=windows GOARCH=$goarch go build -ldflags "-H windowsgui" -o "$filename"
+	  mv "$filename" "$builddir"
+    rm -f "wintun.dll"
   done
 }
 
 
-cd $awlflutterdir
+cd "$awlflutterdir"
 flutter build web --release
 cp -r "$awlflutterdir/build/web" "$awldir/static"
 
@@ -34,7 +47,7 @@ cd "$awldir/cmd/gomobile-lib"
 gomobile bind -o anywherelan.aar -target=android .
 mv anywherelan.aar "$awlflutterdir/android/app/src/main/libs/"
 
-cd $awlflutterdir
+cd "$awlflutterdir"
 flutter build apk --release
 mv "$awlflutterdir/build/app/outputs/flutter-apk/app-release.apk" "$builddir/awl-android-multiarch-$version.apk"
 
