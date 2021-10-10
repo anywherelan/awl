a28da0f4b72a153111943ac648e1e27f0eaa6216
Grigory Krasnochub <kgs@ht.ru>
2021-10-10 17:53:21 +0300

    build script improvements now stable with windows git Bash temp dir based on system preferences some comments some renaming

--- build.sh
+++ build.sh
@@ -3,17 +3,31 @@
 awldir=$(pwd)
 builddir="$awldir/build"
 awlflutterdir="$awldir/../awl-flutter"
+tempdir=$(dirname $(mktemp -u))
+
+wintun_version="wintun-0.11"
+
 
 # until https://github.com/golang/go/issues/37475 is implemented
 VERSION=$(git describe --tags --always --abbrev=8 --dirty)
 
-wintun_version="wintun-0.11"
-if [[ ! -e "/tmp/$wintun_version" ]]; then
-  wget "https://www.wintun.net/builds/$wintun_version.zip"
-  unzip "$wintun_version.zip" -d "/tmp/$wintun_version"
-  rm -f "$wintun_version.zip"
-fi
 
+# download dependencies
+download-wintun() {
+  echo "check dependencies"
+  if [[ ! -e "$tempdir/$wintun_version" ]]; then
+    if ! type "wget" > /dev/null; then
+        echo "wget util could not be found. Please install it"
+        exit
+    fi
+    wget "https://www.wintun.net/builds/$wintun_version.zip"
+    unzip "$wintun_version.zip" -d "$tempdir/$wintun_version"
+    rm -f "$wintun_version.zip"
+  fi
+  echo "dependencies are loaded successfully"
+}
+
+# build for linux OS
 gobuild-linux() {
   name="$1"
   for arch in 386 amd64 arm arm64; do
@@ -23,12 +37,13 @@ gobuild-linux() {
   done
 }
 
+# build for windows OS
 gobuild-windows() {
   name="$1"
   for tuple in "386 x86" "amd64 amd64"; do
     goarch=$(echo "$tuple" | cut -f1 -d" ")
     wintunarch=$(echo "$tuple" | cut -f2 -d" ")
-    cp "/tmp/$wintun_version/wintun/bin/$wintunarch/wintun.dll" wintun.dll
+    cp "$tempdir/$wintun_version/wintun/bin/$wintunarch/wintun.dll" wintun.dll
 
     filename="$name-windows-$goarch-$VERSION.exe"
     GOOS=windows GOARCH=$goarch go build -trimpath -ldflags "-s -w -H windowsgui -X github.com/anywherelan/awl/config.Version=${VERSION}" -o "$filename"
@@ -39,12 +54,14 @@ gobuild-windows() {
 
 # Commands
 
+# create new build dir, delete static dir
 clean() {
   rm -rf build/
   mkdir build
   rm -rf static/
 }
 
+# build flutter web
 build-web() {
   cd "$awldir"
   rm -rf static/
@@ -53,6 +70,7 @@ build-web() {
   cp -r "$awlflutterdir/build/web" "$awldir/static"
 }
 
+# build mobile library
 build-mobile-lib() {
   cd "$awldir/cmd/gomobile-lib"
   go get -d golang.org/x/mobile/cmd/gomobile
@@ -62,33 +80,41 @@ build-mobile-lib() {
   mv anywherelan.aar "$awlflutterdir/android/app/src/main/libs/"
 }
 
+# build for android, require mobile lib
 build-mobile-apk() {
   cd "$awlflutterdir"
   flutter build apk --release
   mv "$awlflutterdir/build/app/outputs/flutter-apk/app-release.apk" "$builddir/awl-android-multiarch-$VERSION.apk"
 }
 
+# build for android
 build-mobile() {
   build-mobile-lib
   build-mobile-apk
 }
 
+# build server version
 build-awl() {
   cd "$awldir/cmd/awl"
   gobuild-linux awl
   gobuild-windows awl
 }
 
+# build desktop version for windows and others OS
 build-awl-tray() {
   cd "$awldir/cmd/awl-tray"
   gobuild-windows awl-tray
-  build-awl-tray-linux-crosscompile
+  build-awl-tray-cur-env-crosscompile
 }
 
-build-awl-tray-linux() {
+# build desktop version based on current environment
+build-awl-tray-cur-env() {
   goos="$(go env GOOS)"
   arch="$(go env GOARCH)"
   filename="awl-tray-$goos-$arch-$VERSION"
+  if [ "$goos" == "windows" ] ;then
+    filename="$filename.exe"
+  fi
   cd "$awldir/cmd/awl-tray"
   go build -trimpath -ldflags "-s -w -X github.com/anywherelan/awl/config.Version=${VERSION}" -o "$filename"
   # set host's rights because when build from docker it will be root:root
@@ -98,13 +124,14 @@ build-awl-tray-linux() {
   mv "$filename" "$builddir"
 }
 
-build-awl-tray-linux-crosscompile() {
+build-awl-tray-cur-env-crosscompile() {
   cd "$awldir"
   for arch in 386 amd64 arm arm64; do
-    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp "awl-cross-$arch" /bin/sh -c './build.sh awl-tray-linux'
+    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp "awl-cross-$arch" /bin/sh -c './build.sh awl-tray-cur-env'
   done
 }
 
+# build server and desktop versions
 build-desktop() {
   build-awl
   build-awl-tray
@@ -119,6 +146,7 @@ build-docker-images() {
 case "${1:-default}" in
 release)
   clean
+  download-wintun
   build-web
   build-mobile
   build-desktop
@@ -133,13 +161,16 @@ mobile)
   build-mobile
   ;;
 desktop)
+  download-wintun
   build-desktop
   ;;
-awl-tray-linux-crosscompile)
-  build-awl-tray-linux-crosscompile
+awl-tray-cur-env-crosscompile)
+  download-wintun
+  build-awl-tray-cur-env-crosscompile
   ;;
-awl-tray-linux)
-  build-awl-tray-linux
+awl-tray-cur-env)
+  download-wintun
+  build-awl-tray-cur-env
   ;;
 docker-images)
   build-docker-images
