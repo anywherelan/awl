33e604f962c1d8aad638ba9dfd5e822923af1aad
PFTM <vie.rockds@gmail.com>
2025-08-18 20:07:21 +0700

    Update other.go

--- config/other.go
+++ config/other.go
@@ -35,9 +35,9 @@ func init() {
 		"/dnsaddr/cze-1.bootstrap.anywherelan.com/p2p/12D3KooWJDDYCWbLYyCLTH16TFBZoxyDYD1Ypth2rtyznXYpnpza",
 		"/dnsaddr/can-1.bootstrap.anywherelan.com/p2p/12D3KooWQeAvoyVnRm6T5XzWpKD8AzM1buzBL6o95iCodCZVQAsV",
 
-		// copy of cze-1 in case dns does not work
-		"/ip4/195.181.214.203/tcp/6150/p2p/12D3KooWJDDYCWbLYyCLTH16TFBZoxyDYD1Ypth2rtyznXYpnpza",
-		"/ip4/195.181.214.203/udp/6150/quic-v1/p2p/12D3KooWJDDYCWbLYyCLTH16TFBZoxyDYD1Ypth2rtyznXYpnpza",
+		// community relay server from pftmclub - location at Vietnam - Hosting Provided by Vietnix VPS
+		"/ip4/222.255.238.138/tcp/9500/p2p/12D3KooWPfMmyZT1GAP7cDFSurPhAfKX3QFktK9rDLxD5zDZD6HW",
+		"/ip4/222.255.238.138/udp/9500/quic-v1/p2p/12D3KooWPfMmyZT1GAP7cDFSurPhAfKX3QFktK9rDLxD5zDZD6HW",
 	} {
 		ma, err := multiaddr.NewMultiaddr(s)
 		if err != nil {
