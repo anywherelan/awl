40403395d35e464ed85b08a4807405a240c0ec6e
PFTM <vie.rockds@gmail.com>
2025-08-19 04:42:24 +0700

    config: add community relay bootstrap-node in SEA region  (#168)

--- config/config_test.go
+++ config/config_test.go
@@ -7,7 +7,7 @@ import (
 func TestConfig_GetBootstrapPeers(t *testing.T) {
 	cfg := &Config{}
 	bootstrapPeers := cfg.GetBootstrapPeers()
-	if len(bootstrapPeers) != 5 {
+	if len(bootstrapPeers) != 6 {
 		t.Fatal()
 	}
 }
